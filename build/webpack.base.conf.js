7837c3912585e1daa025b53fe86c903fbc6279c8
a121266no1a <905862579@qq.com>
2018-10-27 15:15:46 +0800

    第二次

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,8 +25,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      'vue$': 'vue/dist/vue.js',
+      'src': resolve('src'),
+      'common':resolve('src/common')
     }
   },
   module: {
